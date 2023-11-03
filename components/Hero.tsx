07f34727f26fa3a2edd1b4592a2246eb50658cfc
Nahuel Chawi Rojas <nadaro@outlook.com>
2023-11-03 16:59:57 -0300

    camp component

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -4,7 +4,7 @@ import { Button } from './Button';
 
 export const Hero = () => {
   return (
-    <section className="max-container padding-container flex flex-col gap-20 py-10 pb-32 md:gap-28 lg:py-20 xl:flex-row border-2 border-red-500">
+    <section className="max-container padding-container flex flex-col gap-20 py-10 pb-32 md:gap-28 lg:py-20 xl:flex-row ">
       <div className="hero-map" />
       {/* LEFT */}
 
@@ -54,7 +54,7 @@ export const Hero = () => {
           />
         </div>
       </div>
-      <div className="relative flex flex-1 items-start border-2 border-blue-500">
+      <div className="relative flex flex-1 items-start ">
         <div className="relative z-20 w-[268px] flex-col gap-8 rounded-3xl bg-green-90 px-7 py-8">
           <div className="flex flex-col">
             <div className="flexBetween">
