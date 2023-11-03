67773be24b805455240611d46250f38255bf1006
Nahuel Chawi Rojas <nadaro@outlook.com>
2023-11-03 11:03:02 -0300

    ad ded: button component

---
+++ components/Button.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Button = ({type,title,icon,variant}: ButtonProps) => {
+  return (
+    <button
+    type="button"
+    title=""
+    >Button</button>
+  )
+}
