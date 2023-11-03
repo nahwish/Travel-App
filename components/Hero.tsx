b07c81d17a9cd6e36e9642c7479a8d1e0f5b68ba
Nahuel Chawi Rojas <nadaro@outlook.com>
2023-11-03 11:32:24 -0300

    added: hero section image

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,7 +1,22 @@
+import Image from 'next/image'
 import React from 'react'
 
 export const Hero = () => {
   return (
-    <div>Hero</div>
+    <section className="max-container padding-container flex flex-col gap-20 py-10 pb-32 md:gap-28 lg:py-20 xl:flex-row border-2 border-red-500">
+      <div className="hero-map"/>
+      {/* LEFT */}
+
+      <div className="relative z-20 flex flex-1 flex-col xl:w-1/2">
+        <Image
+        src="/camp.svg"
+        alt="camp"
+        width={50}
+        height={50}
+        className="absolute left-[-5px] top-[-30px] w-10 lg:w-[50px]"
+        />
+        <h1 className="bold-52 lg:bold-88">Putuk truno Camp Area</h1>
+      </div>
+    </section>
   )
 }
