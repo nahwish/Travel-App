ed935e0398ca91ca00ae5503308ddef96bb92db6
Nahuel Chawi Rojas <nadaro@outlook.com>
2023-11-03 11:49:19 -0300

    added: button hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image'
 import React from 'react'
+import { Button } from './Button';
 
 export const Hero = () => {
   return (
@@ -23,19 +24,28 @@ export const Hero = () => {
         </p>
         <div className="my-11 flex flex-wrap gap-5">
           <div className="flex items-center gap-2">
-            {
-              Array(5).fill(1).map((_,index)=>(
+            {Array(5)
+              .fill(1)
+              .map((_, index) => (
                 <Image
-                src="/star.svg"
-                key={index}
-                alt="star"
-                width={24}
-                height={24}
+                  src="/star.svg"
+                  key={index}
+                  alt="star"
+                  width={24}
+                  height={24}
                 />
-              ))
-            }
+              ))}
           </div>
-
+          <p className="bold-16 lg:bold-20 text-blue-70">
+            136k
+            <span className="regular-16 lg:regular-20 ml-1 underline">
+              Excelentes Reviews
+            </span>
+          </p>
+        </div>
+        <div className="flex flex-col w-full gap-3 sm:flex-row">
+          <Button type="button" title="Descargá la App" variant="btn_green" />
+          <Button type="button" title="Sobre nosotros" variant="btn_white_text" icon="/play.svg"/>
         </div>
       </div>
     </section>
