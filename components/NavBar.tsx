3c037881848b08d3ed94a65a9cec6e18ea69fbcc
Nahuel Chawi Rojas <nadaro@outlook.com>
2023-11-03 10:54:46 -0300

    added: folder constants

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,3 +1,4 @@
+import { NAV_LINKS } from '@/constants';
 import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
@@ -5,9 +6,18 @@ import React from 'react';
 
 const NavBar = () =>{
   return (
-    <nav className="border-2 border-red-500 flexBetween max-container padding-container relative z-30 py-5">
+    <nav className="flexBetween max-container padding-container relative z-30 py-5">
       <Link href="/">
         <Image src="/hilink-logo.svg" alt="logo" width={74} height={29}/>
+        <ul className="hidden h-full gap-12 lg:flex">
+          {
+            NAV_LINKS.map((link)=>(
+              <Link href={link.href} key={link.key}>
+              {link.label}
+              </Link>
+            ))
+          }
+        </ul>
       </Link>
     </nav>
   )
