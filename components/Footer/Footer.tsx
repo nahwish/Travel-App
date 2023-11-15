53d8a1b7b02f68993f814876d74a52500b79ed05
Nahuel Chawi Rojas <nadaro@outlook.com>
2023-11-15 11:12:19 -0300

    deleted: border

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -58,7 +58,7 @@ export const Footer = () => {
           </div>
         </div>
 
-        <div className="border bg-gray-20" />
+        <div className=" bg-gray-20" />
         <p className="regular-14 w-full text-center text-gray-30">
           2023 Hilink | Todos los derechos reservados
         </p>
